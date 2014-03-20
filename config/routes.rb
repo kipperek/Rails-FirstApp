ae342292fae090882b4cc783e5dee748031c9d2b
Kacper Czechowicz <kipperek@gmail.com>
2014-03-21 00:09:41 +0100

    Graphics + footer

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 MyGists::Application.routes.draw do
   resources :gists
   root :to => 'gists#index'
-
+  get "gists/find"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
